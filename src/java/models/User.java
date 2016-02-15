274b6b8ab569e51f6711a69d064c5ca8ddd4a310
Vladyslav Shyshkin <shyshkin_vlad@yahoo.com>
2016-02-15 19:51:38 +0200

    Add ajax

---
+++ src/java/models/User.java
@@ -0,0 +1,85 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author shyshkin_vlad
+ * @param name имя
+ * @param surname фамилия
+ * @param country страна
+ * @param sex пол
+ * @param sexSelector перечень пола
+ * @param language язык
+ */
+@ManagedBean(name = "User")
+public class User {
+    private String name;
+    private String surname;
+    private String age;
+    private String country;
+    private String sex;
+    private String language;
+    List<String> sexSelector;
+    
+    public User(){
+        sexSelector = new ArrayList<>();
+        sexSelector.add("male");
+        sexSelector.add("female");
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public List<String> getSexSelector() {
+        return sexSelector;
+    }    
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }    
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+    
+}
